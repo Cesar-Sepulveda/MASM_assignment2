e8dd90282e68f63177816dd20d5bea6891472566
Cesar-Sepulveda <35970314+Cesar-Sepulveda@users.noreply.github.com>
2019-02-13 19:30:42 -0800

    Update assignment2.asm

--- assignment2.asm
+++ assignment2.asm
@@ -14,23 +14,23 @@ ExitProcess PROTO, dwExitCode: DWORD
 
 .data
 	bigEndian BYTE 12h, 34h, 56h, 78h			;setting up the big endian variable
-	littleEndian DWORD ?						;setting up the little endian variable
+	littleEndian DWORD ?					;setting up the little endian variable
 
 .code
 
 main PROC
-	mov al, [bigEndian]							;Moving the last 8 bits (al) from the first byte in bigEndian (12h)
-	mov BYTE [littleEndian + 3], al				;Moving te value of al into the fourth address of littleEndian
+	mov ah, [bigEndian]					;Moving the last 8 bits (ah) from the first byte in bigEndian (12h)
+	mov BYTE [littleEndian + 3], ah				;Moving te value of al into the fourth address of littleEndian
 
-	mov al, [bigEndian + 1]						;Moving the last 8 bits (al) from the second byte in bigEndian (34h)
-	mov BYTE [littleEndian + 2], al				;Moving te value of al into the third address of littleEndian
+	mov ah, [bigEndian + 1]					;Moving the last 8 bits (ah) from the second byte in bigEndian (34h)
+	mov BYTE [littleEndian + 2], ah				;Moving te value of al into the third address of littleEndian
 
-	mov al, [bigEndian + 2]						;Moving the last 8 bits (al) from the third byte in bigEndian (56h)
-	mov BYTE  [littleEndian + 1], al			;Moving te value of al into the second address of littleEndian
+	mov ah, [bigEndian + 2]					;Moving the last 8 bits (ah) from the third byte in bigEndian (56h)
+	mov BYTE  [littleEndian + 1], ah			;Moving te value of al into the second address of littleEndian
 
-	mov al, [bigEndian + 3]						;Moving the last 8 bits (al) from the fourth byte in bigEndian (78h)
-	mov BYTE [littleEndian], al					;Moving te value of al into the first address of littleEndian
+	mov ah, [bigEndian + 3]					;Moving the last 8 bits (ah) from the fourth byte in bigEndian (78h)
+	mov BYTE [littleEndian], ah				;Moving te value of al into the first address of littleEndian
 
-	INVOKE ExitProcess, 0						;Could not figure out what was causing the syntax error
+	INVOKE ExitProcess, 0					;Could not figure out what was causing the syntax error
 main ENDP
 END main
